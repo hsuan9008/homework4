828d6fe265d7d9692b3f72824837807b10b7e49e
hsuan9008 <b0829032@cgu.edu.tw>
2021-05-11 21:06:51 +0800

    hw4

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HW4 from './hw4'
+
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          {HW4()}
+        </div>
+    </div>
+  );
+}
+
+export default App;
